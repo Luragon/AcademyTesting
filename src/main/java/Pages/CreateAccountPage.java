9413ade9dc266bd530100152df2ca3f3c0736828
Alan <la_mitologia@hotmail.com>
2021-03-31 16:07:22 -0600

    First Commit Alan

--- src/main/java/Pages/CreateAccountPage.java
+++ src/main/java/Pages/CreateAccountPage.java
@@ -5,6 +5,7 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class CreateAccountPage {
 
@@ -89,15 +90,112 @@ public class CreateAccountPage {
 	@FindBy(id="submitAccount")
 	WebElement registerButton;
 	
-	public void enterName(String name) throws InterruptedException{
-		firstNameTextBox.sendKeys(name);
-		Thread.sleep(3000);
-		
+	public void selectGender(int gender) throws InterruptedException {
+		Select rb1 = new Select(mrRadio);
+		rb1.selectByIndex(gender);
+		Thread.sleep(2000);
 	}
 	
-	public void enterLastName(String name) throws InterruptedException{
+	public void selectGender2(int gender2) throws InterruptedException {
+		Select rb2 = new Select(mrsRadio);
+		rb2.selectByIndex(gender2);
+		Thread.sleep(2000);
+	}
+	
+	
+	public void enterName(String name) throws InterruptedException {
+		firstNameTextBox.sendKeys(name);
+		Thread.sleep(2000);
+	}
+	public void enterLastName(String name) throws InterruptedException {
 		lastNameTextBox.sendKeys(name);
-		Thread.sleep(3000);
+		Thread.sleep(2000);
+	}
+	
+	
+	public void enterEmail(String name) throws InterruptedException {
 		
+		//emailTextBox.equals();
+		emailTextBox.clear();
+		emailTextBox.sendKeys(name);
+		Thread.sleep(2000);
+	}
+	
+	
+	public void enterPassword(String name) throws InterruptedException {
+		passwordTextBox.sendKeys(name);
+		Thread.sleep(2000);
+	}
+	
+	public void dayBirth(String day) throws InterruptedException {
+		Select dropdown = new Select(dayDropdown);
+		dropdown.selectByValue(day);
+		Thread.sleep(2000);
+	}
+	public void monthBirth(int month) throws InterruptedException {
+		Select dropdown = new Select(monthDropdown);
+		dropdown.selectByIndex(month);
+		Thread.sleep(2000);
+	}
+	public void yearBirth(String year) throws InterruptedException {
+		Select dropdown = new Select(yearDropdown);
+		dropdown.selectByValue(year);
+		Thread.sleep(2000);
+	}
+	public void enterAddress1FirstName(String name) throws InterruptedException {
+		addressFirstNameTextBox.clear();
+		addressFirstNameTextBox.sendKeys(name);
+		Thread.sleep(2000);
+	}
+	public void enterAddress1LastName(String name) throws InterruptedException {
+		addressLastNameTextBox.clear();
+		addressLastNameTextBox.sendKeys(name);
+		Thread.sleep(2000);
+	}
+	public void enterCompany(String CompName) throws InterruptedException {
+		companyTextBox.sendKeys(CompName);
+		Thread.sleep(2000);
+	}
+	public void enterAddress(String address) throws InterruptedException {
+		addressTextBox.sendKeys(address);
+		Thread.sleep(2000);
+	}
+	public void enterAddress2(String address2) throws InterruptedException {
+		address2TextBox.sendKeys(address2);
+		Thread.sleep(2000);
+	}
+	public void enterCity(String city) throws InterruptedException {
+		cityTextBox.sendKeys(city);
+		Thread.sleep(2000);
+	}
+	public void enterState(String state) throws InterruptedException {
+		Select dropdown = new Select(stateDropdown);
+		dropdown.selectByVisibleText(state);
+		Thread.sleep(2000);
+	}
+	public void enterPostalCode(String postal) throws InterruptedException {
+		zipCodeTextBox.sendKeys(postal);
+		Thread.sleep(2000);
+	}
+	public void enterAdditional(String addInfo) throws InterruptedException {
+		additionalInfoTextBox.sendKeys(addInfo);
+		Thread.sleep(2000);
+	}
+	public void enterHomePhone(String homePhone) throws InterruptedException {
+		homePhoneTextBox.sendKeys(homePhone);
+		Thread.sleep(2000);
+	}
+	public void enterMobilePhone(String mobilePhone) throws InterruptedException {
+		mobilePhoneTextBox.sendKeys(mobilePhone);
+		Thread.sleep(2000);
+	}
+	public void enterAddressAlias(String addressAlias) throws InterruptedException {
+		addressAliasTextBox.clear();
+		addressAliasTextBox.sendKeys(addressAlias);
+		Thread.sleep(2000);	
+	}
+	public void finishAccount() throws InterruptedException {
+		registerButton.click();
+		Thread.sleep(2000);	
 	}
 }
---
+++ src/test/java/CreateAccountTest.java
@@ -0,0 +1,122 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Pages.AuthenticationPage;
+import Pages.CreateAccountPage;
+import Pages.MainPage;
+
+public class CreateAccountTest {
+
+WebDriver driver;
+	
+	@BeforeTest
+	public void setBaseURL() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:/Users/Soular/Documents/chromedriver_win32/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+	}
+	
+
+	@Test
+	public void Sign_Up() throws InterruptedException {
+		MainPage mainpage = PageFactory.initElements(driver, MainPage.class);
+		AuthenticationPage authPage = PageFactory.initElements(driver, AuthenticationPage.class);
+		
+		//Click a sign in button
+		mainpage.openSignInPage();
+		
+		//Ingresa correo a registrar
+		authPage.enterRegisterEmail("test3team2.com");
+		
+		//Click a crear cuenta
+		authPage.clickCreateAccountButton();
+		}
+
+	
+	@Test
+	public void fillInformation() throws InterruptedException {
+
+	CreateAccountPage createAccPage = PageFactory.initElements(driver, CreateAccountPage.class);
+			//Choose Gender PENDING
+			//createAccPage.selectGender(1);
+			
+			//Choose Gender PENDING
+			//createAccPage.selectGender2(2);
+			
+			//Input First Name
+			createAccPage.enterName("Alan");
+			
+			//Input Last Name
+			createAccPage.enterLastName("Test");
+			
+			//Input Email 
+			createAccPage.enterEmail("test@team2.com");
+			
+			//Input Last Name
+			createAccPage.enterPassword("12345");
+			
+			//Input day Date of Birth 
+			createAccPage.dayBirth("24");
+			
+			//Input month Date of Birth 
+			createAccPage.monthBirth(10);
+			
+			//Input year Date of Birth 
+			createAccPage.yearBirth("1990");
+			
+			//Input Address1 First Name
+			createAccPage.enterAddress1FirstName("Alan");
+
+			//Input Address1 Last Name
+			createAccPage.enterAddress1LastName("Test");
+			
+			//Input Company Name
+			createAccPage.enterCompany("");
+			
+			//Input Address1
+			createAccPage.enterAddress("Test Street");
+			
+			//Input Address2
+			createAccPage.enterAddress2("");
+			
+			//Input City Name
+			createAccPage.enterCity("Test City");
+			
+			//Input State
+			createAccPage.enterState("Connecticut");
+			
+			//Input State
+			createAccPage.enterPostalCode("68934");
+
+			//Input Additional Info
+			createAccPage.enterAdditional("Additional Info Test");
+			
+			//Input Home Phone Info
+			createAccPage.enterHomePhone("8181878564");
+
+			//Input Mobile Phone Info
+			createAccPage.enterMobilePhone("8181878564");
+			
+			//Input Additional Info
+			createAccPage.enterAddressAlias("My Adress Test");
+			
+			//Sign Up
+			createAccPage.finishAccount();
+	}
+	
+	
+	@AfterTest
+	public void endSession() {
+		driver.quit();
+	}
+}
+
