b12fe4f649b791176de0278f803087c31e9c3d92
Mibalo <kevinibarraloera@gmail.com>
2021-03-31 17:11:08 -0600

    commit Kevin Ibarra

--- src/main/java/Pages/MainPage.java
+++ src/main/java/Pages/MainPage.java
@@ -38,6 +38,54 @@ public class MainPage {
 	@FindBy(xpath="//*[@class='product_list grid row']/li[1]/div/div[@class='right-block']/div[@class='button-container']/a")
 	WebElement AddToCartS;	
 
+	@FindBy(xpath="//div[@class='product-container']")
+	WebElement dress;
+	
+	@FindBy(xpath="//button[@class='exclusive']")
+	WebElement cart;
+	
+	@FindBy(xpath="//span[@class='continue btn btn-default button exclusive-medium']")
+	WebElement continueshopping;
+	
+	@FindBy(xpath="/html/body/div/div[2]/div/div[3]/div/p[2]/a[1]")
+	WebElement checkouttwo;
+	
+	@FindBy(xpath="/html/body/div/div[2]/div/div[3]/div/form/p/button/span")
+	WebElement checkoutthree;
+	
+	@FindBy(xpath="/html/body/div/div[2]/div/div[3]/div/div/form/p/button")
+	WebElement checkoutfour;
+
+
+	@FindBy(xpath="//a[@class='ajax_cart_block_remove_link']")
+	WebElement removeitem;
+	
+	@FindBy(xpath="//a[@class='btn btn-default button button-medium']")
+	WebElement checkout;
+	
+	@FindBy(xpath="//i[@class='icon-trash']")
+	WebElement trash;
+	
+	@FindBy(id ="email")
+	WebElement emailTextbox;
+	
+	@FindBy(id = "passwd")
+	WebElement passwordTextbox;
+	 	
+	@FindBy(id = "SubmitLogin")
+	WebElement Submit;
+	
+	@FindBy(xpath="/html/body/div/div[2]/div/div[3]/div/div/form/div/p[2]/div/span/input")
+	WebElement termsCheckbox;
+	
+	@FindBy(xpath="/html/body/div/div[2]/div/div[3]/div/div/div[3]/div[1]/div/p/a")
+	WebElement bankWire;
+	
+	@FindBy(xpath="/html/body/div/div[2]/div/div[3]/div/form/p/button/span")
+	WebElement confirmBankPayment;
+	
+	@FindBy(xpath="/html/body/div/div[2]/div/div[3]/div/form/p/button/span")
+	WebElement check;
 	
 	public void addProductFromHome() throws InterruptedException {
 		AddToCartH.click();
@@ -64,15 +112,6 @@ public class MainPage {
 		ContinueShopping.click();
 		Thread.sleep(3000);
 	}
-
-	public void searchClothes(String clothes) throws InterruptedException {
-		searchBox.sendKeys(clothes);
-		Thread.sleep(1000);
-		searchButton.click();
-		Thread.sleep(3000);
-	}
-	
-
 	
 	public void goHome() throws InterruptedException {
 		logoImg.click();
@@ -83,4 +122,97 @@ public class MainPage {
 		signIn.click();
 		Thread.sleep(1000);
 	}
+	
+	
+	public void searchClothes(String clothes) throws InterruptedException {
+		searchBox.sendKeys(clothes);
+		Thread.sleep(500);
+		searchButton.click();
+	}
+	
+	public void writeEmail(String email) throws InterruptedException {
+		emailTextbox.sendKeys(email);
+		Thread.sleep(500);
+	}
+	
+
+	public void writePassword(String password) throws InterruptedException {
+		passwordTextbox.sendKeys(password);
+		Thread.sleep(500);
+	}
+	
+	public void Submitbtn() throws InterruptedException {
+		Submit.click();
+	}
+	
+	
+	
+	//Los void que agrege yo
+	
+	public void dress() throws InterruptedException {
+		dress.click();
+		Thread.sleep(5000);
+	}
+	
+	public void cart() throws InterruptedException {
+		cart.click();
+		Thread.sleep(1500);
+	}
+	
+	public void continueshopping() throws InterruptedException {
+		continueshopping.click();
+		Thread.sleep(500);
+	}
+	
+	public void removeitem() throws InterruptedException {
+		removeitem.click();
+		Thread.sleep(500);
+	}
+	
+	public void checkout() throws InterruptedException {
+		checkout.click();
+		Thread.sleep(500);
+	}
+	
+	public void checkouttwo() throws InterruptedException {
+		checkouttwo.click();
+		Thread.sleep(500);
+	}
+	
+	public void checkouthree() throws InterruptedException {
+		checkoutthree.click();
+		Thread.sleep(500);
+	}
+	
+	public void checkoutfour() throws InterruptedException {
+		checkoutfour.click();
+		Thread.sleep(500);
+	}
+	
+	public void bankwire() throws InterruptedException {
+		bankWire.click();
+		Thread.sleep(500);
+	}
+	
+	public void check() throws InterruptedException {
+		bankWire.click();
+		Thread.sleep(500);
+	}
+	
+	public void confirm() throws InterruptedException {
+		confirmBankPayment.click();
+		Thread.sleep(500);
+	}
+	
+	
+	public void trash() throws InterruptedException {
+		trash.click();
+		Thread.sleep(500);
+	}
+	
+	public void agreeToTerms() throws InterruptedException {
+		termsCheckbox.click();
+		Thread.sleep(500);
+	}
+	
 }
---
+++ src/test/java/SelectPaymentCheck.java
@@ -0,0 +1,80 @@
+
+import java.util.concurrent.TimeUnit;
+
+
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Pages.MainPage;
+
+public class SelectPaymentCheck {
+
+WebDriver driver;
+	
+	@BeforeTest
+	public void setBaseURL() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:/Users/Work/Downloads/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		Thread.sleep(3000);
+	}
+	
+	@Test
+	public void payment() throws InterruptedException{
+		//Step 1. El Usuario busca un vestido
+		MainPage mainpage = PageFactory.initElements(driver, MainPage.class);
+		
+		mainpage.searchClothes("dress");
+				
+		//Step 2 El Usuario da click a un vestido 
+		mainpage.dress();
+				
+		//Step 3 El usuario da click a agregar al carrito
+		mainpage.cart();
+		
+		//Step 4 El usuario da click en proceed to checkout
+		mainpage.checkout();
+		
+		//Step 5 El usuario da click en proceed to checkout
+		mainpage.checkouttwo();
+		
+		//Step 6 El usuario ingresa su correo
+		mainpage.writeEmail("kevinibarraloera@gmail.com");
+		
+		// Step 7 El usuario ingresa su password
+		mainpage.writePassword("dragonfist98");
+		
+		// Step 8 El usuario inicia sesion dando click en submit
+		mainpage.Submitbtn();
+		
+		//Step 9 El usuario da click en proceed to checkout
+		mainpage.checkouthree();
+		
+		//Step 10 El usuario da click en el checkbox donde acepta los terminos
+		mainpage.agreeToTerms();
+		
+		//Step 11 El usuario da click en proceed to checkout
+		mainpage.checkoutfour();
+		
+		//Step 12 El usuario selecciona cheque como opcion de compra
+		mainpage.check();
+		
+		//Step 12 El usuario confirma
+		mainpage.confirm();
+							
+	}
+		
+		
+	@AfterTest
+public void endSession() {
+driver.quit();
+}
+}
